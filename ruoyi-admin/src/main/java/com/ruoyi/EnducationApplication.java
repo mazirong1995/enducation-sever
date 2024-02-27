ee4fc6507cce375759249dec0a371c07b4a27bc0
Ma <1064120164@qq.com>
2024-02-27 23:49:26 +0800

    修改启动类名

--- ruoyi-admin/src/main/java/com/ruoyi/EnducationApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/EnducationApplication.java
@@ -10,21 +10,19 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
+public class EnducationApplication
 {
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
+        SpringApplication.run(EnducationApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  在线教育启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |       \\  _. /  '       \n" +
                 " |(_ o _) /        _( )_ .'         \n" +
                 " | (_,_).' __  ___(_ o _)'          \n" +
                 " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+                " |  | \\ `'   /|   `-'  /           \n"
+               );
     }
 }
